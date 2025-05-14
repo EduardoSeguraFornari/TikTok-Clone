9fabcf35c615638eb5bc0789ee445e27f3c85591
fornari_eduardo <fornari_eduardo@sicredi.com.br>
2025-05-14 18:46:02 -0300

    adding NotificationsView

---
+++ TikTok-Clone/Core/Notifications/NotificationCell.swift
@@ -0,0 +1,43 @@
+//
+//  NotificationCell.swift
+//  TikTok-Clone
+//
+//  Created by Eduardo Segura Fornari on 14/05/25.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundStyle(Color(.systemGray5))
+
+            HStack {
+                Text("max.verstapen")
+                    .font(.footnote)
+                    .fontWeight(.semibold) +
+
+                Text(" liked one of your posts. ")
+                    .font(.footnote) +
+
+                Text("3d")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+
+            Spacer()
+
+            Rectangle()
+                .frame(width: 48, height: 48)
+                .clipShape(RoundedRectangle(cornerRadius: 6))
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    NotificationCell()
+}
---
+++ TikTok-Clone/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationsView.swift
+//  TikTok-Clone
+//
+//  Created by Eduardo Segura Fornari on 14/05/25.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 24) {
+                    ForEach(0..<20) { notification in
+                        NotificationCell()
+                    }
+                }
+            }
+            .navigationTitle("Notifications")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.top)
+        }
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
