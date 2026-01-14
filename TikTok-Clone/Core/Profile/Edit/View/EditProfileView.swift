ee0ba3b30ffdaeb3a181307cd16d3165e7bc58d8
fornari_eduardo <fornari_eduardo@sicredi.com.br>
2026-01-14 15:59:14 -0300

    selecting profile image with PhotosPicker

--- TikTok-Clone/Core/Profile/Edit/View/EditProfileView.swift
+++ TikTok-Clone/Core/Profile/Edit/View/EditProfileView.swift
@@ -6,20 +6,35 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditProfileView: View {
+    @State private var selectedPickerItem: PhotosPickerItem?
+    @State private var profileImage: Image?
+
     var body: some View {
         NavigationStack {
             VStack {
-                VStack {
-                    Circle()
-                        .frame(width: 64, height: 64)
-                    
-                    Button("Change photo") {
-                        print("DEBUG: Change photo")
+                PhotosPicker(selection: $selectedPickerItem, matching: .images) {
+                    VStack {
+                        if let image = profileImage {
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 64, height: 64)
+                                .clipShape(Circle())
+                        } else {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 64, height: 64)
+                                .clipShape(Circle())
+                                .foregroundStyle(Color(.systemGray4))
+                        }
+                        Text("Change photo")
                     }
-                    .foregroundStyle(.black)
                 }
+                .buttonStyle(.plain)
                 .padding()
                 
                 VStack(alignment: .leading, spacing: 24) {
@@ -51,6 +66,9 @@ struct EditProfileView: View {
                 
                 Spacer()
             }
+            .task(id: selectedPickerItem) {
+                await loadImage(fromItem: selectedPickerItem)
+            }
             .navigationTitle("Edit Profile")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
@@ -69,6 +87,15 @@ struct EditProfileView: View {
                 }
             }
         }
+    }
+}
+
+extension EditProfileView {
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item else { return }
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.profileImage = Image(uiImage: uiImage)
     }
 }
 
